69c2fad67eee6410b7021bee4268c8c6dab1c15f
RKCbnits <ravindra.kumar@cbnits.com>
2021-02-07 19:15:30 +0530

    TradingView Chart

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
 import React, {Component} from 'react';
-import {AppRegistry,ToastAndroid} from 'react-native';
+import {AppRegistry,} from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class app extends Component {
-    jsToInject = `
-    tvWidget.onChartReady(function() {
-        tvWidget.chart().onIntervalChanged().subscribe(
-            null,
-            function(interval) {
-                const response = { type: "onIntervalChanged", interval: interval }
-                //window.ReactNativeWebView.postMessage accepts one argument, data,
-                //which will be available on the event object, event.nativeEvent.data. data must be a string.
-                window.ReactNativeWebView.postMessage(JSON.stringify(response));
-            }
-        );
-    });
-    true; // note: this is required, or you'll sometimes get silent failures
-          // (https://github.com/react-native-webview/react-native-webview/blob/master/docs/Guide.md)
-  `;
   render() {
     return (
           <WebView
-          style={{flex: 1,width:'100%',height:'100%'}}
+          style={{flex: 1}}
           originWhitelist={["*"]}
-          source={{ uri: 'file:///android_asset/index.html' }}
+          source={{uri:'https://coinbase-assignment.herokuapp.com/'}}
           javaScriptEnabled={true}
           domStorageEnabled={true}
           allowFileAccessFromFileURLs={true}
           allowFileAccess={true}
           allowUniversalAccessFromFileURLs={true}
-          injectedJavaScript={this.jsToInject}
-          // onShouldStartLoadWithRequest={() => false}
-          // onMessage={(event) => {
-          //     const data = JSON.parse(event.nativeEvent.data)
-          //     if (data.type == "onIntervalChanged") {
-          //         ToastAndroid.show("Interval = " + data.interval, ToastAndroid.SHORT);
-          //     }
-          // }}
       />
     );
   }
